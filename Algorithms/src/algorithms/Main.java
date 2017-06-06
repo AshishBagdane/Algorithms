102f0b03c7e931e14c08f37c75df233bcd764b0a
Ashish <Ashish@192.168.1.6>
2017-06-06 14:46:05 +0530

    Creating Algorithms Repository

--- Algorithms/src/algorithms/Main.java
+++ Algorithms/src/algorithms/Main.java
@@ -3,6 +3,8 @@ package algorithms;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Creating algorithms repository !");
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("Creating algorithms repository !");
+		}
 	}
 }
