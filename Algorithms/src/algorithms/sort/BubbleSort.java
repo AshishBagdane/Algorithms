561f1b98d7855896efc474b341cefd50831a9131
Ashish <Ashish@192.168.1.6>
2017-06-07 16:13:59 +0530

    Methods ascending and descending added for String class

--- Algorithms/src/algorithms/sort/BubbleSort.java
+++ Algorithms/src/algorithms/sort/BubbleSort.java
@@ -73,4 +73,28 @@ public class BubbleSort {
 			}
 		}
 	}
+	
+	public static void ascending(String[] arr) {
+		for (Integer i = 0; i < arr.length - 1; i++) {
+			for (Integer j = i + 1; j < arr.length; j++) {
+				if (arr[i].compareTo(arr[j]) > 0) {
+					String temp = arr[j];
+					arr[j] = arr[i];
+					arr[i] = temp;
+				}
+			}
+		}
+	}
+	
+	public static void descending(String[] arr) {
+		for (Integer i = 0; i < arr.length - 1; i++) {
+			for (Integer j = i + 1; j < arr.length; j++) {
+				if (arr[i].compareTo(arr[j]) < 0) {
+					String temp = arr[j];
+					arr[j] = arr[i];
+					arr[i] = temp;
+				}
+			}
+		}
+	}
 }
