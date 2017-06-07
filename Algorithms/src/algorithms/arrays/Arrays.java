394427a3b72308b36b3641120eb6ea35f7b03326
Ashish <Ashish@192.168.1.6>
2017-06-07 15:52:48 +0530

    Methods findMin and findMax created

--- Algorithms/src/algorithms/arrays/Arrays.java
+++ Algorithms/src/algorithms/arrays/Arrays.java
@@ -4,14 +4,14 @@ public class Arrays {
 
 	public static <E> void print(E[] arr) {
 		for (E element : arr) {
-			System.out.print(element + " ");
+			System.out.print(element.toString() + " ");
 		}
 		System.out.println();
 	}
 	
 	public static <E> void printReverse(E[] arr) {
 		for (int i = arr.length - 1; i >= 0; i--) {
-			System.out.print(arr[i] + " ");
+			System.out.print(arr[i].toString() + " ");
 		}
 		System.out.println();
 	}
@@ -25,29 +25,69 @@ public class Arrays {
 		}
 	}
 	
-	public static int findMax(int[] arr) {
-		int max = Integer.MIN_VALUE;
-		for (int element : arr) {
-			if (element > max) {
+	public static Integer findMax(Integer[] arr) {
+		Integer max = Integer.MIN_VALUE;
+		for (Integer element : arr) {
+			if (element.compareTo(max) > 0) {
 				max = element;
 			}
 		}
 		return max;
 	}
 	
-	public static int findMin(int[] arr) {
-		int min = Integer.MAX_VALUE;
-		for (int element : arr) {
-			if (element < min) {
+	public static Integer findMin(Integer[] arr) {
+		Integer min = Integer.MAX_VALUE;
+		for (Integer element : arr) {
+			if (element.compareTo(min) < 0) {
 				min = element;
 			}
 		}
 		return min;
 	}
 	
-	public static long sum(int[] arr) {
-		long sum = 0;
-		for (int element : arr) {
+	public static Long findMax(Long[] arr) {
+		Long max = Long.MIN_VALUE;
+		for (Long element : arr) {
+			if (element.compareTo(max) > 0) {
+				max = element;
+			}
+		}
+		return max;
+	}
+	
+	public static Long findMin(Long[] arr) {
+		Long min = Long.MAX_VALUE;
+		for (Long element : arr) {
+			if (element.compareTo(min) < 0) {
+				min = element;
+			}
+		}
+		return min;
+	}
+	
+	public static Character findMax(Character[] arr) {
+		Character max = Character.MIN_VALUE;
+		for (Character element : arr) {
+			if (element.compareTo(max) > 0) {
+				max = element;
+			}
+		}
+		return max;
+	}
+	
+	public static Character findMin(Character[] arr) {
+		Character min = Character.MAX_VALUE;
+		for (Character element : arr) {
+			if (element.compareTo(min) < 0) {
+				min = element;
+			}
+		}
+		return min;
+	}
+	
+	public static Long sum(Long[] arr) {
+		Long sum = 0L;
+		for (Long element : arr) {
 			sum += element;
 		}
 		return sum;
