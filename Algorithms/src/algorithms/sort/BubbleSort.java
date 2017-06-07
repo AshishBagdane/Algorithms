58dea1a9aa126c7648f9badcc7ce46fe7b99a63e
Ashish <Ashish@192.168.1.6>
2017-06-07 15:17:42 +0530

    Ascending and Descending

---
+++ Algorithms/src/algorithms/sort/BubbleSort.java
@@ -0,0 +1,28 @@
+package algorithms.sort;
+
+public class BubbleSort {
+
+	public static void ascending(int[] arr) {
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] > arr[j]) {
+					int temp = arr[j];
+					arr[j] = arr[i];
+					arr[i] = temp;
+				}
+			}
+		}
+	}
+	
+	public static void descending(int[] arr) {
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] < arr[j]) {
+					int temp = arr[j];
+					arr[j] = arr[i];
+					arr[i] = temp;
+				}
+			}
+		}
+	}
+}
